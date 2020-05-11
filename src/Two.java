064b14c5f973146699f02d378fbec2dfc0f71f40
abhi <akrulzz04@gmail.com>
2020-05-11 13:40:29 +0530

    Tried implementing lombok

--- src/Two.java
+++ src/Two.java
@@ -2,7 +2,7 @@ import mypack.One;
 
 public class Two {
     public static void main(String[] args) {
-        One obj=new One(17085006,"Abhishek");
-        System.out.println(obj.getName()+" "+obj.getId());
+        One obj= One.builder().id(123).build();
+        System.out.println(obj.getId());
     }
 }
--- src/mypack/One.java
+++ src/mypack/One.java
@@ -1,15 +1,12 @@
 package mypack;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigInteger;
 @AllArgsConstructor
 @Data
+@Builder
 public class One {
-    public Integer id;
-    public String name;
+    public int id;
 
 }
